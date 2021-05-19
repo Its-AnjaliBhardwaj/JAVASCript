9c1490f1bd842f2073f8c356836a4138f24a217c
Its-AnjaliBhardwaj <anjali16bhardwaj@gmail.com>
2021-05-19 17:26:28 +0530

    First Commit

---
+++ basics.js
@@ -0,0 +1,20 @@
+// The most basics of JS:****** Single line comment.*****
+
+/*Variable intialization: *****Multiple line of comments.*****
+
+Type 1: In this  kind of intialization, we can declare and reassign both.*/ 
+var x = 100;
+var x = "True";
+
+console.log(x);
+console.log(x);
+
+//Type 2 : In this kind of intialization,
+ //we can't reassign .
+let y = 100;
+console.log(y);
+
+//Type 3 : In this kind of intialization,  we neither can redeclare nor reassign . 
+const z = "Hello Java Script";
+
+console.log(z);
